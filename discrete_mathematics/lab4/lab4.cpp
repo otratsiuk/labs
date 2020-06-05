1184d95f6a3741cf988b03d617d89e2551810c7d
Olga Tratsiuk <lelikiris@gmail.com>
2020-06-05 22:25:45 +0300

    Update lab

--- discrete_mathematics/lab4/lab4.cpp
+++ discrete_mathematics/lab4/lab4.cpp
@@ -83,7 +83,7 @@ string ZhegalkinPolynomial(int x[], int y[], int z[], int f[]) {
   return polynomial;
 }
 
-void PostClasses(int f[]) {
+void PostClasses(int x[], int y[], int z[], int f[]) {
   if (f[0] == 0) {
     cout << "zero saving function\n";
   } else
@@ -92,6 +92,38 @@ void PostClasses(int f[]) {
     cout << "unit saving function\n";
   } else
     cout << "not unit saving function\n";
+  string polynomial = ZhegalkinPolynomial(x, y, z, f);
+  int count = 0;
+  for (int i = 0; i < polynomial.length(); i++) {
+    if (polynomial[i] == '^')
+      count++;
+  }
+  string conjunctions[count + 1];
+  for (int i = 0, j = 0; i < polynomial.length(), j < count + 1; i++) {
+    if (polynomial[i] != '^')
+      conjunctions[j].push_back(polynomial[i]);
+    else
+      j++;
+  }
+  bool linearity = 1;
+  int power_count = 0;
+  for (int i = 0; i < count + 1; i++) {
+    power_count = 0;
+    for (int j = 0; j < conjunctions[i].length(); j++) {
+      if (conjunctions[i][j] == 'x' || conjunctions[i][j] == 'y' ||
+          conjunctions[i][j] == 'z') {
+        power_count++;
+      }
+      if (conjunctions[i][j] > 1)
+        linearity = 0;
+    }
+    if (power_count > 1)
+      linearity = 0;
+  }
+  if (linearity == 0) {
+    cout << "not linear function\n";
+  } else
+    cout << "linear function\n";
   if (f[7] >= f[6] && f[6] >= f[2] && f[2] >= f[0] && f[7] >= f[5] &&
       f[5] >= f[4] && f[5] >= f[1] && f[4] >= f[0] && f[1] >= f[0] &&
       f[7] >= f[3] && f[3] >= f[2]) {
@@ -99,12 +131,25 @@ void PostClasses(int f[]) {
   } else
     cout << "not monotonous function\n";
   if (f[0] != f[7] && f[1] != f[6] && f[2] != f[5] && f[3] != f[4]) {
-    cout << "self-dual function\n\n";
+    cout << "self-dual function\n";
   } else
     cout << "not self-dual function\n\n";
 }
 
-main() {
+void DualFunction(int x[], int y[], int z[], int f[]) {
+  int f2[8];
+  for (int i = 0; i < 8; i++) {
+    f2[i] = !f[i];
+  }
+  cout << "\ntruth table for dual function: \n\n"
+       << "x y z   f\n";
+  for (int i = 0, j = 7; i < 8; i++, j--) {
+    cout << x[i] << " " << y[i] << " " << z[i] << "   " << f2[j] << "\n";
+  }
+  cout << "\n";
+}
+
+int main() {
   int x[8] = {0, 0, 0, 0, 1, 1, 1, 1}, y[8] = {0, 0, 1, 1, 0, 0, 1, 1},
       z[8] = {0, 1, 0, 1, 0, 1, 0, 1}, f[8];
   for (int i = 0; i < 8; i++) {
@@ -115,15 +160,12 @@ main() {
   for (int i = 0; i < 8; i++) {
     cout << x[i] << " " << y[i] << " " << z[i] << "   " << f[i] << "\n";
   }
-  cout << "\n"
-       << "SDNF: \n"
-       << sdnf(x, y, z, f) << "\n";
-  cout << "\n"
-       << "SKNF: \n"
-       << sknf(x, y, z, f) << "\n";
-  cout << "\n"
-       << "Zhegalkin polynomial: \n"
+  cout << "\nSDNF: \n";
+  cout << sdnf(x, y, z, f) << "\n";
+  cout << "\nSKNF: \n" << sknf(x, y, z, f) << "\n";
+  cout << "\nZhegalkin polynomial: \n"
        << ZhegalkinPolynomial(x, y, z, f) << "\n";
   cout << "\n";
-  PostClasses(f);
+  PostClasses(x, y, z, f);
+  DualFunction(x, y, z, f);
 }
