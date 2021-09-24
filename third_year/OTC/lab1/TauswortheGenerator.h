07e65b56b77a36029a41b7ad27b8d4f63d87560f
Olga Tratsiuk <otratsiuk@gmail.com>
2021-09-24 13:01:40 +0300

    Do OTC lab1

--- third_year/OTC/lab1/TauswortheGenerator.h
+++ third_year/OTC/lab1/TauswortheGenerator.h
@@ -14,15 +14,23 @@ private:
     std::vector<float> X;
 
 private:
+    void fillFirstComponents()
+    {
+        unsigned seed {static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count())};
+        std::default_random_engine engine(seed);
+        std::uniform_int_distribution<int> distb(0, 1);
+
+        for (auto i = Bo.begin(); i != Bo.begin() + q; i++)
+        {
+            *i = distb(engine);
+        }
+    }
+
     void fillSecondComponents(){
         Bo.erase(Bo.begin() + q, Bo.end());
-        std::cout<<"\n";
+
         for (int i = q; i < 2 * q; i++){
             Bo.push_back((static_cast<int>(Bo[i - r]) + static_cast<int>(Bo[i - q])) % 2);
-        }
-
-        for(auto b : Bo){
-            std::cout << b << " ";
         }
     }
 
@@ -30,7 +38,7 @@ private:
         uint base = 1;
         uint dec = 0;
 
-        for(auto i = Bo.rbegin(); i != Bo.rend(); ++i)
+        for(auto i = Bo.rbegin(); i != Bo.rend() - q; ++i)
         {
             dec += *i * base;
             base = base * 2; 
@@ -46,7 +54,7 @@ public:
         unsigned seed {static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count())};
         std::default_random_engine engine(seed);
 
-        std::uniform_int_distribution<int> distq(1, 30);
+        std::uniform_int_distribution<int> distq(1, 31);
         q = distq(engine);
 
         std::uniform_int_distribution<int> distr(1, q);
@@ -56,26 +64,22 @@ public:
 
         std::uniform_int_distribution<int> distb(0, 1);
 
-        std::cout<<"\nM = " << M <<"\n";
         for (int i = 0; i < q; i++)
         {
             Bo.push_back(distb(engine));
-            std::cout<<Bo[i]<<" ";
         }
     }
 
     std::vector<float> random(int n){
-        fillSecondComponents();
 
-        uint x = fromBinToDec();
-        X.push_back(static_cast<float>(x)/M);
-        std::cout<<"\n"<< x << "\n";
-        std::cout<<X[0]<<"\n";
-
+        uint x;
+        for(int i = 0; i < n; i++)
+        {
+            fillSecondComponents();
+            x = fromBinToDec();
+            X.push_back(static_cast<float>(x)/M);
+            fillFirstComponents();
+        }
         return X;
     }
-
-
-
-
 };
--- third_year/OTC/lab1/main.cpp
+++ third_year/OTC/lab1/main.cpp
@@ -1,9 +1,38 @@
 #include "TauswortheGenerator.h"
+#include <iomanip>
 
 int main()
 {
-    Tausworthe generator;
-    generator.random(4);
+    Tausworthe generator100;
+    Tausworthe generator500;
+    Tausworthe generator1000;
+    Tausworthe generator10000;
+    Tausworthe generator30000;
+    
+    auto vector100   = generator100.random(100);
+    auto vector500   = generator500.random(500);
+    auto vector1000  = generator1000.random(1000);
+    auto vector10000 = generator10000.random(10000);
+    auto vector30000 = generator30000.random(30000); 
+
+    std::cout<<std::left
+             <<std::setw(15)<<"100"
+             <<std::setw(15)<<"500"
+             <<std::setw(15)<<"1000"
+             <<std::setw(15)<<"10000"
+             <<std::setw(15)<<"30000\n";
+    
+    for(int i = 0; i < 50; i++){
+        std::cout<<"\n"
+                 <<std::left
+                 <<std::setw(15)<<vector100[i]
+                 <<std::setw(15)<<vector500[i]
+                 <<std::setw(15)<<vector1000[i]
+                 <<std::setw(15)<<vector10000[i]
+                 <<std::setw(15)<<vector30000[i];
+    }
+
+
 
     return 0;
 }
